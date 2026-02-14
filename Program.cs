044aafcb581a1fff769adc4ddb74bb64134249e9
AnitahWairimu <tita.wairimu@gmail.com>
2026-02-14 21:59:41 +0300

    Update console output and logic in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to C# beginner toolkit");
+
+        int number = 7; // You can change this number
+
+        if (number > 5)
+        {
+            Console.WriteLine("The number is greater than five");
+        }
+        else
+        {
+            Console.WriteLine("Any key to exit");
+        }
+
+        Console.ReadKey(); // Keeps console open
+    }
+}
+
+
+
+
